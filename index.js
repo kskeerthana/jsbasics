23cbe84b53907c258a986c89508cae1ae7ffe6fe
keerthanaks <keerks07@gmail.com>
2020-08-27 23:29:20 +0530

    check

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log('Wazaaaaappp!');
+let name = 'John';
+const value=1.23 
+console.log(name);
+//string 
+const message = 'heya';
+const text = new String('hola!')//string object
+//object 
+let person={
+    name:'John',
+    age:21
+};
+console.log(person);
+//dot notation 
+console.log(person.age);
+
+//array
+let array=['name','age'];
+console.log(array.length);
+
+//function
+function hello(name){
+    console.log('Hey ' + name)
+}
+hello('Midge');
+
+function square(num){
+    return num*num;
+}
+console.log(square(3));
