3dcdfbe70e795793447106f0b2a3e916b38b9707
keerthanaks <keerks07@gmail.com>
2020-08-29 16:19:44 +0530

    update

--- index.js
+++ index.js
@@ -28,3 +28,36 @@ function square(num){
     return num*num;
 }
 console.log(square(3));
+//if-else
+let hour=19;
+if (hour>=6 && hour<12)
+    console.log('Good morning');
+else if  (hour>=12 && hour<18)
+    console.log('Good Afternoon');
+else
+    console.log('Good Evening');   
+
+//array map
+const colors=["red","blue","green"];
+//const items= colors.map(color => '<li>' + color + '</li>');
+const items= colors.map(color => `'<li>'${color}'</li>'`);
+console.log(items);
+
+//spread operator
+const first={name : 'joshh'};
+const second={job:'teacher'};
+const combined = {...first, age:40, ...second};
+console.log(combined);
+
+//class
+
+class People{
+    constructor(name){
+        this.name=name;
+    }
+    walk(){
+        console.log('walk');
+    }
+}
+const peep=new People('KS');
+console.log(peep);
